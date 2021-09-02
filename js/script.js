5766cbf3552bbf5051e3c9e01a0c3d41399359f3
Artem Kolakovskyi <kolakovskyiartem@gmail.com>
2021-09-02 17:25:32 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+let x = 4;
+let y = 9;
+let r = 10;
+Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= r ? console.log('Точка находится внутри окружности') : console.log('Точка находится за окружностю');
+
